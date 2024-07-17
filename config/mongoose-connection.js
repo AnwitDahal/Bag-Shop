56718fb65572cb074ee071cbf888467fbb7a9c10
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-18 00:45:58 +0545

    environment variable set, admin route created

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
+const config = require("config");
+
+const dbgr = require("debug")("development:mongoose");
 
 mongoose
-  .connect(`mongodb://localhost:27017/bagshop`)
+  .connect(`${config.get("MONGODB_URI")}/bagshop`)
   .then(() => {
-    console.log("Database has been connected");
+    // console.log("Database has been connected");
+    dbgr("Database has been connected");
   })
   .catch((err) => {
-    console.log(`Database has not been connected because of ${err}`);
+    // console.log(`Database has not been connected because of ${err}`);
+    dbgr(`Database has not been connected because of ${err}`);
   });
 
 module.exports = mongoose.connection;
--- routes/adminsRouter.js
+++ routes/adminsRouter.js
@@ -1,8 +1,27 @@
-const express=require('express')
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const adminModel = require("../models/admin-models");
 
-router.get('/',(req,res)=>{
-    res.send('hello admin')
-})
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let admin = await adminModel.find();
+    if (admin.length > 0)
+      return res
+        .send(504)
+        .send("You dont have permission to create a new admin");
+        let {fullname,email,password}=req.body 
+       let createdAdmin= await adminModel.create({
+            fullname,
+            email,
+            password,
+        })
+    res.status(201).send(createdAdmin);
 
-module.exports=router;
+  });
+}
+
+router.get("/", (req, res) => {
+  res.send("hello admin");
+});
+
+module.exports = router;
