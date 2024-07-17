a4da033793e0f2d4ee7e25b81d03a6591c00da16
AnwitDahal <dahalanwit0707@gmail.com>
2024-07-17 19:22:38 +0545

    Database Model Schema

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const path=require('path')
+const app=express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(3000)
